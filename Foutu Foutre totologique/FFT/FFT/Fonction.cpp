5244258faf5d8bf821f4d09dc24e5203abce9184
simon <simonlerou@gmail.com>
2021-02-19 22:20:55 -0500

    creation des fichiers

---
+++ Foutu Foutre totologique/FFT/FFT/Fonction.cpp
@@ -0,0 +1,14 @@
+#include "Fonction.h"
+
+void FFT(float sample[])
+{
+}
+
+void MM(float sample[])
+{
+}
+
+int peek(float sample[])
+{
+    return 0;
+}
---
+++ Foutu Foutre totologique/FFT/FFT/Fonction.h
@@ -0,0 +1,7 @@
+#pragma once
+void FFT(float sample[]);
+void MM(float sample[]);
+int  peek(float sample[]);
+
+
+
