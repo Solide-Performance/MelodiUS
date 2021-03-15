2801d5036874941f5c380fc0725196858660a02b
Raesangur <pascalpinguin@gmail.com>
2021-03-14 22:44:20 -0400

    No SIMD in FFTs D:

--- MelodiUS/source/fft.cpp
+++ MelodiUS/source/fft.cpp
@@ -9,59 +9,15 @@
 
 #include "benchmark.h"
 
+#ifndef LINUX_
+#include <immintrin.h>
+#include <xmmintrin.h>
+#endif
+
 
 /*****************************************************************************/
 /* Constants --------------------------------------------------------------- */
 constexpr int MAX_DEPTH = 4;
-
-
-/*****************************************************************************/
-/* Classes ----------------------------------------------------------------- */
-class OddVector
-{
-public:
-    constexpr static int even = 0;
-    constexpr static int odd  = 1;
-
-private:
-    std::vector<complex_t>& m_originalVector;
-    int                     m_depth     = 1;
-    int                     m_oddOrEven = even;
-
-public:
-    OddVector() = delete;
-    OddVector(OddVector& other, int oddOrEven)
-    : m_originalVector{other.m_originalVector}, m_oddOrEven{oddOrEven}, m_depth{other.m_depth + 1}
-    {
-        assert(oddOrEven == odd || oddOrEven == even);
-    }
-    OddVector(std::vector<complex_t>& otherVec, int oddOrEven)
-    : m_originalVector{otherVec}, m_oddOrEven{oddOrEven}
-    {
-        assert(oddOrEven == odd || oddOrEven == even);
-    }
-
-    [[nodiscard]] complex_t& operator[](size_t index)
-    {
-        return m_originalVector[index * 2 * m_depth + m_oddOrEven];
-    }
-
-    [[nodiscard]] std::vector<complex_t>::iterator begin()
-    {
-        return m_originalVector.begin();
-    }
-    [[nodiscard]] std::vector<complex_t>::iterator end()
-    {
-        return m_originalVector.end();
-    }
-
-    [[nodiscard]] size_t size()
-    {
-        return m_originalVector.size() % (2 * m_depth) == 0
-                 ? m_originalVector.size() / (2 * m_depth)
-                 : m_originalVector.size() / (2 * m_depth) + 1;
-    }
-};
 
 
 /*****************************************************************************/
@@ -99,51 +55,6 @@ double FindFrequency(const Recording& audio)
     return freq;
 }
 
-void FFT_op(OddVector& x, int depth)
-{
-    if(x.size() <= 1)
-    {
-        return;
-    }
-
-    // divide
-    Benchmark* copy = nullptr;
-    OddVector  even(x, OddVector::even);
-    OddVector  odd(x, OddVector::odd);
-    delete copy;
-
-    // conquer
-    if(depth > MAX_DEPTH)
-    {
-        FFT_op(even, depth + 1);
-        FFT_op(odd, depth + 1);
-    }
-    else
-    {
-        std::thread evenThread{FFT_op, std::ref(even), depth + 1};
-        // std::thread oddThread{FFT, std::ref(odd), depth};
-        FFT_op(odd, depth + 1);
-
-        evenThread.join();
-        // oddThread.join();
-    }
-
-    // combine
-    Benchmark* combine = nullptr;
-    if(depth < MAX_DEPTH)
-    {
-        combine = new Benchmark{"combine"};
-    }
-#pragma omp parallel for
-    for(size_t k = 0; k < x.size() / 2; ++k)
-    {
-        complex_t t         = std::polar(1.0, -2 * pi * k / x.size()) * odd[k];
-        x[k]                = even[k] + t;
-        x[k + x.size() / 2] = even[k] - t;
-    }
-    delete combine;
-}
-
 // https://rosettacode.org/wiki/Fast_Fourier_transform#C.2B.2B
 void FFT(std::vector<complex_t>& x, int depth)
 {
@@ -153,33 +64,37 @@ void FFT(std::vector<complex_t>& x, int depth)
     }
 
     // divide
-    Benchmark* copy = nullptr;
-    OddVector  even(x, OddVector::even);
-    OddVector  odd(x, OddVector::odd);
-    delete copy;
+    const size_t           elements = x.size() % 2 == 0 ? x.size() / 2 : x.size() / 2 + 1;
+    std::vector<complex_t> even(elements);
+    std::vector<complex_t> odd(elements);
+
+    /* clang-format off */
+    // For vector slicing in even and odd pairs
+    auto lmbd = [N = false](const complex_t& c) mutable
+                {
+                    (void)c;
+                    return N = !N;
+                };
+    /* clang-format on */
+
+    std::copy_if(x.begin(), x.end(), even.begin(), lmbd);
+    std::copy_if(x.begin() + 1, x.end(), odd.begin(), lmbd);
 
     // conquer
     if(depth > MAX_DEPTH)
     {
-        FFT_op(even, depth + 1);
-        FFT_op(odd, depth + 1);
+        FFT(even, depth + 1);
+        FFT(odd, depth + 1);
     }
     else
     {
-        std::thread evenThread{FFT_op, std::ref(even), depth + 1};
-        // std::thread oddThread{FFT, std::ref(odd), depth};
-        FFT_op(odd, depth + 1);
+        std::thread evenThread{FFT, std::ref(even), depth + 1};
+        FFT(odd, depth + 1);
 
         evenThread.join();
-        // oddThread.join();
     }
 
     // combine
-    Benchmark* combine = nullptr;
-    if(depth < MAX_DEPTH)
-    {
-        combine = new Benchmark{"combine"};
-    }
 #pragma omp parallel for
     for(size_t k = 0; k < x.size() / 2; ++k)
     {
@@ -187,7 +102,6 @@ void FFT(std::vector<complex_t>& x, int depth)
         x[k]                = even[k] + t;
         x[k + x.size() / 2] = even[k] - t;
     }
-    delete combine;
 }
 
 
