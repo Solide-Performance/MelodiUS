fef767fde81b32ba53f659c506661e96099be452
Pascal-Emmanuel Lachance <lacp3102@usherbrooke.ca>
2021-03-18 11:05:12 -0400

    extincteur à feu

--- MelodiUS/source/detection_rythme.cpp
+++ MelodiUS/source/detection_rythme.cpp
@@ -34,16 +34,16 @@ int analyse_rythme(const Recording& rec)
     std::vector<float> tableau = rec.getSamples();
     size_t             taille  = tableau.size();
 
-    std::vector<float>  derive(taille);
-    std::vector<float>  volume(taille);
-    std::vector<float>  volume_plat(taille);
-    std::vector<float>  derive_double(taille);
-    std::vector<int>  debut_note(taille);
-    std::vector<int>  fin_note(taille);
-    float               maximum = 0;
-    const float marge_bruit=0.0005;
-    const float marge_volume=0.005;
-    const float marge_note
+    std::vector<float> derive(taille);
+    std::vector<float> volume(taille);
+    std::vector<float> volume_plat(taille);
+    std::vector<float> derive_double(taille);
+    std::vector<int>   debut_note(taille);
+    std::vector<int>   fin_note(taille);
+    float              maximum      = 0;
+    const float        marge_bruit  = 0.0005;
+    const float        marge_volume = 0.005;
+    const float        marge_note   = 0.5f;
 
     for(size_t i = 0; i < taille - 1; i++)
     {
@@ -52,52 +52,46 @@ int analyse_rythme(const Recording& rec)
 
     for(size_t i = 0; i < taille - 1; i++)
     {
-        if(COMPARE_FLOATS(derive[i], 0.0f, epsilon) && tableau[i] > /*marge_volume*/ )
+        if(COMPARE_FLOATS(derive[i], 0.0f, epsilon) && tableau[i] > 0/*marge_volume*/)
         {    // comparaison avec marge d'erreur, utilise la fonction de pascal
             maximum = tableau[i];
         }
         volume[i] = maximum;
     }
 
-    for(size_t i = 0; i< taille; i++)
+    for(size_t i = 0; i < taille; i++)
     {
-        if(volume[i]<marge_bruit)
+        if(volume[i] < marge_bruit)
         {
-            volume[i]=0;
+            volume[i] = 0;
         }
     }
-    
-    float  volume_moyen = 0.f;
-    for(int i=0; i<taille; i+=dt)
+
+    float volume_moyen = 0.f;
+    for(int i = 0; i < taille; i += dt)
     {
-        volume_moyen=0;
-        
-    
-        for(int j=0 ; j < std::min(i + dt, volume.size()); j++)
+        volume_moyen = 0;
+
+
+        for(int j = 0; j < std::min(i + dt, volume.size()); j++)
         {
-                volume_moyen += volume[j];
+            volume_moyen += volume[j];
         }
         volume_moyen /= dt;
-        std::fill(&volume_plat[i], &volume_plat[i+dt], volume_moyen);
-
+        std::fill(&volume_plat[i], &volume_plat[i + dt], volume_moyen);
     }
-    int compteur_debut=0;
+    int compteur_debut = 0;
     for(size_t i = 0; i < taille - 1; i++)
     {
         derive_double[i] = volume_plat[i + 1] - volume_plat[i];
-        if(derive_double[i]>marge_note)
+        if(derive_double[i] > marge_note)
         {
-            debut_note[compteur_fin]=i;
+            debut_note[compteur_debut] = i;
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
     return 0;
 }
 
