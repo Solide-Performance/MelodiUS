6251d05bcd2c2cfa52b66a67816a311df01959b2
Alexandre Bernier <bera1107@usherbrooke.ca>
2021-04-06 14:08:42 -0400

    aaaaaaaaargh

--- MelodiUS/include/mainwindow_ui.h
+++ MelodiUS/include/mainwindow_ui.h
@@ -15,8 +15,6 @@
 #include <QtWidgets/QGroupBox>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
-#include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
@@ -27,11 +25,7 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QAction* actionNouveau;
-    QAction* actionImporter;
-    QAction* actionEnregistrer;
-    QAction* actionG_n_rer_un_sinus;
-    QAction* actionPlayback;
+   
     QWidget* centralwidget;
 
     QLabel* label;
@@ -52,144 +46,104 @@ public:
     QPushButton* buttonRecord;
     QPushButton* buttonStopRecord;
 
-    QMenuBar*   menubar;
-    QMenu*      menuFichier;
-    QMenu*      menuOutil;
-    QStatusBar* statusbar;
+  
 
     void setupUi(QMainWindow* mainWindow)
     {
-        if(mainWindow->objectName().isEmpty())
-            mainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        mainWindow->resize(1600, 900);    // Window size
-        actionNouveau = new QAction(mainWindow);
-        actionNouveau->setObjectName(QString::fromUtf8("actionNouveau"));
-        actionImporter = new QAction(mainWindow);
-        actionImporter->setObjectName(QString::fromUtf8("actionImporter"));
-        actionEnregistrer = new QAction(mainWindow);
-        actionEnregistrer->setObjectName(QString::fromUtf8("actionEnregistrer"));
-        actionG_n_rer_un_sinus = new QAction(mainWindow);
-        actionG_n_rer_un_sinus->setObjectName(QString::fromUtf8("actionG_n_rer_un_sinus"));
-        actionPlayback = new QAction(mainWindow);
-        actionPlayback->setObjectName(QString::fromUtf8("actionplayback"));
+       // if(mainWindow->objectName().isEmpty())
+        //    mainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        mainWindow->resize(1600, 900);    
         centralwidget = new QWidget(mainWindow);
-        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+      //  centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
-        label->setObjectName(QString::fromUtf8("label"));
+       // label->setObjectName(QString::fromUtf8("label"));
         label->setGeometry(QRect(4, 489, 851, 91));
         label_3 = new QLabel(centralwidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
+       // label_3->setObjectName(QString::fromUtf8("label_3"));
         label_3->setGeometry(QRect(10, 130, 51, 101));
         label_3->setPixmap(QPixmap(QString::fromUtf8("../../MelodiUS/meloiusImage/cle40x80T.png")));
 
         ///=== LIGNE HORIZONTALE ========//
         line = new QFrame(centralwidget);
-        line->setObjectName(QString::fromUtf8("line"));
+       // line->setObjectName(QString::fromUtf8("line"));
         line->setGeometry(QRect(10, 130, 1600, 21));
         line->setFrameShape(QFrame::HLine);
         line->setFrameShadow(QFrame::Sunken);
         line_2 = new QFrame(centralwidget);
-        line_2->setObjectName(QString::fromUtf8("line_2"));
+       // line_2->setObjectName(QString::fromUtf8("line_2"));
         line_2->setGeometry(QRect(10, 150, 1600, 21));
         line_2->setFrameShape(QFrame::HLine);
         line_2->setFrameShadow(QFrame::Sunken);
         line_3 = new QFrame(centralwidget);
-        line_3->setObjectName(QString::fromUtf8("line_3"));
+       // line_3->setObjectName(QString::fromUtf8("line_3"));
         line_3->setGeometry(QRect(10, 170, 1600, 21));
         line_3->setFrameShape(QFrame::HLine);
         line_3->setFrameShadow(QFrame::Sunken);
         line_4 = new QFrame(centralwidget);
-        line_4->setObjectName(QString::fromUtf8("line_4"));
+       //line_4->setObjectName(QString::fromUtf8("line_4"));
         line_4->setGeometry(QRect(10, 190, 1600, 21));
         line_4->setFrameShape(QFrame::HLine);
         line_4->setFrameShadow(QFrame::Sunken);
         line_5 = new QFrame(centralwidget);
-        line_5->setObjectName(QString::fromUtf8("line_5"));
+       // line_5->setObjectName(QString::fromUtf8("line_5"));
         line_5->setGeometry(QRect(10, 210, 1600, 21));
         line_5->setFrameShape(QFrame::HLine);
         line_5->setFrameShadow(QFrame::Sunken);
 
         ///=== LIGNE VERTICALE ========//
         line_6 = new QFrame(centralwidget);
-        line_6->setObjectName(QString::fromUtf8("line_6"));
+      //  line_6->setObjectName(QString::fromUtf8("line_6"));
         line_6->setGeometry(QRect(250, 140, 20, 81));
         line_6->setFrameShape(QFrame::VLine);
         line_6->setFrameShadow(QFrame::Sunken);
 
         spinBox = new QSpinBox(centralwidget);
-        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+     //   spinBox->setObjectName(QString::fromUtf8("spinBox"));
         spinBox->setGeometry(QRect(50, 150, 42, 22));
         spinBox->setValue(4);
         spinBox->setRange(1, 8);
         spinBox_2 = new QSpinBox(centralwidget);
-        spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
+      //  spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
         spinBox_2->setGeometry(QRect(50, 190, 42, 22));
         spinBox_2->setValue(4);
         spinBox_2->setRange(2, 8);
         spinBox_2->setSingleStep(2);
 
         pushButtonA = new QPushButton(centralwidget);
-        pushButtonA->setObjectName(QString::fromUtf8("pushButton"));
+      //  pushButtonA->setObjectName(QString::fromUtf8("pushButton"));
         pushButtonA->setGeometry(QRect(70, 370, 93, 28));
 
         buttonRecord = new QPushButton(centralwidget);
-        buttonRecord->setObjectName(QString::fromUtf8("buttonRecord"));
+      //  buttonRecord->setObjectName(QString::fromUtf8("buttonRecord"));
         buttonRecord->setGeometry(QRect(50, 300, 93, 28));
         buttonStopRecord = new QPushButton(centralwidget);
-        buttonStopRecord->setObjectName(QString::fromUtf8("buttonStopRecord"));
+      //  buttonStopRecord->setObjectName(QString::fromUtf8("buttonStopRecord"));
         buttonStopRecord->setGeometry(QRect(50, 300, 93, 28));
         buttonStopRecord->hide();
 
         mainWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(mainWindow);
-        menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 26));
-        menuFichier = new QMenu(menubar);
-        menuFichier->setObjectName(QString::fromUtf8("menuFichier"));
-        menuOutil = new QMenu(menubar);
-        menuOutil->setObjectName(QString::fromUtf8("menuOutil"));
-        mainWindow->setMenuBar(menubar);
-        statusbar = new QStatusBar(mainWindow);
-        statusbar->setObjectName(QString::fromUtf8("statusbar"));
-        mainWindow->setStatusBar(statusbar);
-
-        menubar->addAction(menuFichier->menuAction());
-        menubar->addAction(menuOutil->menuAction());
-        menuFichier->addAction(actionNouveau);
-        menuFichier->addAction(actionImporter);
-        menuFichier->addAction(actionEnregistrer);
-        menuOutil->addAction(actionG_n_rer_un_sinus);
-        menuOutil->addAction(actionPlayback);
+        
 
         retranslateUi(mainWindow);
 
-        // QMetaObject::connectSlotsByName(MainWindow);
-    }    // setupUi
+        
+    }    
 
     void retranslateUi(QMainWindow* MainWindow)
     {
-        MainWindow->setWindowTitle(
-          QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        actionNouveau->setText(QCoreApplication::translate("MainWindow", "Nouveau", nullptr));
-        actionImporter->setText(QCoreApplication::translate("MainWindow", "Importer", nullptr));
-        actionEnregistrer->setText(
-          QCoreApplication::translate("MainWindow", "Enregistrer", nullptr));
-        actionG_n_rer_un_sinus->setText(
-          QCoreApplication::translate("MainWindow", "G\303\251n\303\251rer un sinus", nullptr));
-        actionPlayback->setText(QCoreApplication::translate("MainWindow", "Playback", nullptr));
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+         
         label->setText(QCoreApplication::translate(
           "MainWindow",
           "<html><head/><body><p>MelodiUS V1.0   UwU Solide Performance</p></body></html>",
           nullptr));
-        //  centralwidget->setTitle(
-        //  QCoreApplication::translate("MainWindow", "Affichage partition", nullptr));
+      
         pushButtonA->setText(QCoreApplication::translate("MainWindow", "A", nullptr));
         buttonRecord->setText(QCoreApplication::translate("MainWindow", "Record", nullptr));
         buttonStopRecord->setText(
           QCoreApplication::translate("MainWindow", "Stop Recording", nullptr));
-        menuFichier->setTitle(QCoreApplication::translate("MainWindow", "Fichier", nullptr));
-        menuOutil->setTitle(QCoreApplication::translate("MainWindow", "Outil", nullptr));
-    }    // retranslateUi
+       
+    }    
 };
 
 namespace Ui
