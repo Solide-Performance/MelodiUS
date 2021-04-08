3aed6434eae18bc857e5aa000f7dca68dfd6b80d
Philippe Gauthier <gaup1302@usherbrooke.ca>
2021-04-08 11:42:09 -0400

    les notes sont liés aux symboles

--- MelodiUS/include/detection_rythme.h
+++ MelodiUS/include/detection_rythme.h
@@ -2,12 +2,15 @@
 /*****************************************************************************/
 /* Includes ---------------------------------------------------------------- */
 #include "recording.h"
-
+#include "note.h"
 
 /*****************************************************************************/
 /* Function declarations --------------------------------------------------- */
 std::vector<Recording> analyse_rythme(const Recording& rec);
-void analyse_note(std::vector<size_t> debuts, std::vector<size_t> fins, size_t recordingLength);
+void                   analyse_note(std::vector<size_t>    debuts,
+                                    std::vector<size_t>    fins,
+                                    size_t                 recordingLength,
+                                    std::vector<NoteValue> valeur_note);
 
 
 /*****************************************************************************/
