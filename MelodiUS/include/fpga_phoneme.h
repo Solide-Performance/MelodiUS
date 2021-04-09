dbff9a53893c76fba7ecbc4e2ee6c1510b3b717b
Pascal-Emmanuel Lachance <lacp3102@usherbrooke.ca>
2021-04-09 13:13:32 -0400

    FPGA no longer singleton

--- MelodiUS/include/fpga_phoneme.h
+++ MelodiUS/include/fpga_phoneme.h
@@ -24,75 +24,47 @@ enum class Phoneme : uint8_t
 };
 
 
-/*****************************************************************************/
-/* Class ------------------------------------------------------------------- */
-
-/* The FPGA class is a `static singleton`
-   This code could be out of a class, and within a simple namespace instead. */
-class FPGA
+namespace FPGA
 {
-#ifdef LINUX_
-    static void* m_fpga;
-#else
-    static CommunicationFPGA* m_fpga;
-#endif
-
-    static std::thread*        m_listener;
-    static bool*               m_run;
-    static std::array<int, 4>* m_adc;    // Stuck with `int` because of CommunicationFPGA lib
-    static std::array<std::function<void()>, 5>*        m_phonemeCallbacks;
-    static std::function<void(std::array<uint8_t, 4>)>* m_updateCallback;
-    static Phoneme*                                     m_currentPhoneme;
-    static Phoneme*                                     m_oldPhoneme;
-    static size_t*                                      m_phonemeCounter;
-
-public:
-    static void Init();
-    static void DeInit();
-    static void StartListener();
-
-    /* --------------------------------- */
-    /* Accessors                         */
-    [[nodiscard]] static bool                   isOk();
-    [[nodiscard]] static std::string            ErrorMsg();
-    [[nodiscard]] static std::array<uint8_t, 4> GetADC();
-    [[nodiscard]] static uint8_t                GetADC(size_t channel);
-    [[nodiscard]] static Phoneme                GetCurrentPhoneme();
-    static void SetPhonemeCallback(Phoneme number, std::function<void()> callback);
-    static void SetUpdateCallback(std::function<void(std::array<uint8_t, 4>)> callback);
-
-    static void WriteLED(uint8_t val);
-
-private:
-    /* --------------------------------- */
-    /* Methods                           */
-    FPGA() = default; /* The constructor is private to avoid instanciation */
-
-    static void ListenerThread();
-    static void CheckADCPhonemes();
-    static void CheckButtonPhonemes();
-    static void CallCallback(Phoneme channel);
-    static void DisplayADC();
-
-    /* --------------------------------- */
-    /* Types                             */
-    enum Registers : int
-    {
-        /* Ports, these values might change */
-        BUTTON       = 0,
-        SWITCH       = 1,
-        TIME_COUNTER = 2,
-        ADC0         = 3,
-        ADC1         = 4,
-        ADC2         = 5,
-        ADC3         = 6,
-        AN0          = 7,
-        AN1          = 8,
-        DOT          = 9,
-        LED          = 10,
-    };
+enum Registers : int
+{
+    /* Ports, these values might change */
+    BUTTON       = 0,
+    SWITCH       = 1,
+    TIME_COUNTER = 2,
+    ADC0         = 3,
+    ADC1         = 4,
+    ADC2         = 5,
+    ADC3         = 6,
+    AN0          = 7,
+    AN1          = 8,
+    DOT          = 9,
+    LED          = 10,
 };
 
 
 /*****************************************************************************/
+/* Function declarations - -------------------------------------------------- */
+
+
+void Init();
+void DeInit();
+void StartListener();
+
+/* --------------------------------- */
+/* Accessors                         */
+[[nodiscard]] bool                   isOk();
+[[nodiscard]] std::string            ErrorMsg();
+[[nodiscard]] std::array<uint8_t, 4> GetADC();
+[[nodiscard]] uint8_t                GetADC(size_t channel);
+[[nodiscard]] Phoneme                GetCurrentPhoneme();
+void SetPhonemeCallback(Phoneme number, std::function<void()> callback);
+void SetUpdateCallback(std::function<void(std::array<uint8_t, 4>)> callback);
+
+void WriteLED(uint8_t val);
+
+
+
+/*****************************************************************************/
 /* END OF FILE ------------------------------------------------------------- */
+}    // namespace FPGA
