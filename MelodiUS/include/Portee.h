4a26bcd5781608abf5c60396a541cd42eccc4e43
simon13456 <simonlerou@gmail.com>
2021-04-09 15:59:49 -0400

    Copy constructoir marche pas caliss

--- MelodiUS/include/Portee.h
+++ MelodiUS/include/Portee.h
@@ -35,53 +35,51 @@ public:
       line_10(GroupBox)
     {
         ///=== LIGNE HORIZONTALE ========//
-        int decalage = 150 * n;
-        line.setGeometry(QRect(20, 130 + decalage, 950, 21));
+        
+        line.setGeometry(QRect(20, 130 + (150 * n), 950, 21));
         line.setFrameShape(QFrame::HLine);
         line.setFrameShadow(QFrame::Plain);
 
-        line_2.setGeometry(QRect(20, 150 + decalage, 950, 21));
+        line_2.setGeometry(QRect(20, 150 + (150 * n), 950, 21));
         line_2.setFrameShape(QFrame::HLine);
         line_2.setFrameShadow(QFrame::Plain);
 
-        line_3.setGeometry(QRect(20, 170 + decalage, 950, 21));
+        line_3.setGeometry(QRect(20, 170 + (150 * n), 950, 21));
         line_3.setFrameShape(QFrame::HLine);
         line_3.setFrameShadow(QFrame::Plain);
 
-        line_4.setGeometry(QRect(20, 190 + decalage, 950, 21));
+        line_4.setGeometry(QRect(20, 190 + (150 * n), 950, 21));
         line_4.setFrameShape(QFrame::HLine);
         line_4.setFrameShadow(QFrame::Plain);
 
-        line_5.setGeometry(QRect(20, 210 + decalage, 950, 21));
+        line_5.setGeometry(QRect(20, 210 + (150 * n), 950, 21));
         line_5.setFrameShape(QFrame::HLine);
         line_5.setFrameShadow(QFrame::Plain);
         ///=== LIGNE VERTICALE ========//
 
-        line_6.setGeometry(QRect(10, 140 + decalage, 20, 81));    // debut
+        line_6.setGeometry(QRect(10, 140 + (150 * n), 20, 81));    // debut
         line_6.setFrameShape(QFrame::VLine);
         line_6.setFrameShadow(QFrame::Plain);
 
-        line_7.setGeometry(QRect(237, 140 + decalage, 20, 81));
+        line_7.setGeometry(QRect(237, 140 + (150 * n), 20, 81));
         line_7.setFrameShape(QFrame::VLine);
         line_7.setFrameShadow(QFrame::Plain);
 
-        line_8.setGeometry(QRect(237 * 2, 140 + decalage, 20, 81));
+        line_8.setGeometry(QRect(237 * 2, 140 + (150 * n), 20, 81));
         line_8.setFrameShape(QFrame::VLine);
         line_8.setFrameShadow(QFrame::Plain);
 
-        line_10.setGeometry(QRect(237 * 3, 140 + decalage, 20, 81));
+        line_10.setGeometry(QRect(237 * 3, 140 + (150 * n), 20, 81));
         line_10.setFrameShape(QFrame::VLine);
         line_10.setFrameShadow(QFrame::Plain);
 
-        line_9.setGeometry(QRect(960, 140 + decalage, 20, 81));
+        line_9.setGeometry(QRect(960, 140 + (150 * n), 20, 81));
         line_9.setFrameShape(QFrame::VLine);
         line_9.setFrameShadow(QFrame::Plain);
 
-        Cle.setGeometry(QRect(10, 130 + decalage, 51, 101));
+        Cle.setGeometry(QRect(10, 130 +(150 * n), 51, 101));
         Cle.setPixmap(QPixmap(QString::fromUtf8("images/cle40x80T.png")));
     };
-
-private:
     QFrame line;
     QFrame line_2;
     QFrame line_3;
