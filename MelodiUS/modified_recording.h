96457d9e56c93129609dd6a50ac35083667a5699
simon13456 <simonlerou@gmail.com>
2021-04-10 02:36:16 -0400

    dla marde

--- MelodiUS/modified_recording.h
+++ MelodiUS/modified_recording.h
@@ -9,16 +9,38 @@ class modified_recording : public Recording
 {
 private:
     struct gain;
+    size_t              m_numChannels=0;
     std::vector<SAMPLE> m_samplesModified{};
 public:
     modified_recording(const Recording&);
     ~modified_recording();
+
+    void normalize(int max);
+    void panning(int position);
 };
 /* --------------------------------- */
 modified_recording::modified_recording(const Recording& rec)
 {
-    //m_samplesModified = std::move();
+    
 }
 modified_recording::~modified_recording()
 {
+}
+inline void modified_recording::normalize(int max)
+{
+    if(m_numChannels == 2)
+    {
+
+    }
+    else
+    {
+        int pek = m_samplesModified[0];
+        for(int i = 0; i < m_samplesModified.size(); i++)
+            {
+                if(m_samplesModified[i] > pek)
+                {
+                    pek = m_samplesModified[i];
+                }
+            }
+    }
 }
